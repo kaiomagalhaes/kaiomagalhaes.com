bfccf7d5764eb385dc3f851bc78b9cd64c2a0ba5
Kaio Magalhaes <me@kaiomagalhaes.com>
2022-08-14 23:39:23 +0200

    open social links in new tab

--- src/lib/components/SocialLinks.svelte
+++ src/lib/components/SocialLinks.svelte
@@ -3,13 +3,13 @@
 </script>
 
 <div class="text-center text-secondary flex justify-center gap-4 {className}">
-  <a href="https://twitter.com/kaiocmagalhaes" aria-label="My Twitter profile" class="hover:text-primary">
+  <a href="https://twitter.com/kaiocmagalhaes" aria-label="My Twitter profile" class="hover:text-primary" target="_blank">
     <i class="icon-twitter" />
   </a>
-  <a href="https://github.com/kaiomagalhaes" aria-label="My Github profile" class="hover:text-primary">
+  <a href="https://github.com/kaiomagalhaes" aria-label="My Github profile" class="hover:text-primary" target="_blank">
     <i class="icon-github" />
   </a>
-  <a href="/rss" aria-label="My RSS feed" class="hover:text-primary">
+  <a href="/rss" aria-label="My RSS feed" class="hover:text-primary" target="_blank">
     <i class="icon-rss" />
   </a>
 </div>
