6991aa2d342497a76e1d88a6a98b5bf6606c500d
Kaio Magalhaes <me@kaiomagalhaes.com>
2024-03-06 21:49:24 -0300

    rename nebs to fred

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,12 +4,12 @@
   $: message = $page.error?.message;
   $: is404 = message?.toLowerCase() === 'not found'
   $: title =
-    is404 ? "sorry, nebs couldn't find that page :(" : 'something went wrong';
+    is404 ? "sorry, fred couldn't find that page :(" : 'something went wrong';
 </script>
 
 <div class="flex items-center justify-center p-8">
   <div>
-    <img src="/favicon-16x16.png" alt="The best doggo" class="w-32 m-auto my-8" />
+    <img src="/favicon-16x16.png" alt="The best fox" class="w-32 m-auto my-8" />
     <h1 class="text-xl mb-0">{title}</h1>
 
     {#if message && !is404}
