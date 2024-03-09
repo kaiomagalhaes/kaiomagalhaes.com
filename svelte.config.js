63f1de6c5d8813bcf9287c429bb64b177f74b3b5
Kaio Magalhaes <me@kaiomagalhaes.com>
2024-03-08 22:23:28 -0300

    analytics

--- svelte.config.js
+++ svelte.config.js
@@ -19,6 +19,8 @@ const config = {
           'self',
           'sha256-zfRvLbR3zlaekiwICEa9S3jjc9sLhqYte6tX4ScxNSI=',
           'sha256-weAyrPtk9s1QpDfc5D5fH5bUfhVFNjL0mNVRLvTDoTY=',
+          'sha256-fprJQMnXMmKRU5kI/CI0D2N6U+3VOemzrfSeR/aLyQQ=',
+          'sha512-euoFGowhlaLqXsPWQ48qSkBSCFs3DPRyiwVu3FjR96cMPx+Fr+gpWRhIafcHwqwCqWS42RZhIudOvEI+Ckf6MA=='
         ],
         'connect-src': ['self'],
         'style-src': ['self', 'unsafe-inline', 'https://fonts.googleapis.com'],
