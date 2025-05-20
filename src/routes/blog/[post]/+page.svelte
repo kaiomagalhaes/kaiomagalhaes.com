ba28f6c94682a8fd6f3bc638c0be04291fc2e260
Kaio Magalhães <me@kaiomagalhaes.com>
2025-05-20 19:06:32 -0300

    Add SEO meta tags

--- src/routes/blog/[post]/+page.svelte
+++ src/routes/blog/[post]/+page.svelte
@@ -1,27 +1,31 @@
 <script lang="ts">
   import type { PageData } from './$types';
   import { formatTitle, formatPublishDate } from '$lib/format';
+  import BlogLayout from '$lib/components/BlogLayout.svelte';
   export let data: PageData;
   let title = formatTitle(data.metadata.title);
   let published = formatPublishDate(data.metadata.published);
+  let canonical = `https://www.kaiomagalhaes.com/blog/${data.metadata.slug}`;
 </script>
 
-<span class="text-secondary font-cursive relative -top-8 md:inline hidden">
-  {published}
-</span>
+<BlogLayout title={data.metadata.title} published={data.metadata.published} canonical={canonical}>
+  <span class="text-secondary font-cursive relative -top-8 md:inline hidden">
+    {published}
+  </span>
 
-<h1 class="text-center">{title}</h1>
+  <h1 class="text-center">{title}</h1>
 
-<span class="text-secondary font-cursive relative block text-center text-sm md:hidden -mb-2 -mt-1">
-  {published}
-</span>
+  <span class="text-secondary font-cursive relative block text-center text-sm md:hidden -mb-2 -mt-1">
+    {published}
+  </span>
 
-<hr />
+  <hr />
 
-<svelte:component this={data.component} {published} />
+  <svelte:component this={data.component} {published} />
 
-<hr />
+  <hr />
 
-<div class="text-center text-link pt-8 pb-12">
-  <a href="/"> read some more </a>
-</div>
+  <div class="text-center text-link pt-8 pb-12">
+    <a href="/"> read some more </a>
+  </div>
+</BlogLayout>
